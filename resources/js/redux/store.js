dfb2ffa58b9a98d1f946fff220d999a0e512f43e
midesweb <malvarez@desarrolloweb.com>
2020-07-27 19:44:10 +0200

    redux dev tools

--- resources/js/redux/store.js
+++ resources/js/redux/store.js
@@ -2,6 +2,7 @@ import {
   createStore,
   combineReducers,
   applyMiddleware,
+  compose,
 } from "redux";
 
 import thunk from "redux-thunk";
@@ -9,9 +10,14 @@ import thunk from "redux-thunk";
 import { app } from "./reducers/app_reducers";
 import { user } from "./reducers/user_reducers";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const reducers = combineReducers({
   app,
   user
 });
 
-export const store = createStore(reducers, applyMiddleware(thunk) );
+export const store = createStore(
+  reducers, 
+  composeEnhancers(applyMiddleware(thunk))
+);

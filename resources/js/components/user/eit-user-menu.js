21361e83a12ecf3890a315b2a66517b2a64bffd5
midesweb <malvarez@desarrolloweb.com>
2020-07-27 12:43:52 +0200

    componentes del menú de usuario

--- resources/js/components/user/eit-user-menu.js
+++ resources/js/components/user/eit-user-menu.js
@@ -1,5 +1,7 @@
 import { LitElement, html, css } from 'lit-element';
 import '../forms/eit-button';
+import '@dile/dile-avatar/dile-avatar.js';
+import '@dile/dile-menu-overlay/dile-menu-overlay';
 
 class EitUserMenu  extends LitElement {
 
@@ -8,11 +10,27 @@ class EitUserMenu  extends LitElement {
       :host {
         display: block;
       }
+      dile-avatar {
+        --dile-avatar-color: var(--secondary-color, #333);
+      }
+      .trigger {
+        cursor: pointer;
+      }
+      .menucontent {
+        padding: 0.8rem;
+      }
+      .menucontent p {
+        margin-top: 0;
+        padding-bottom: 10px;
+        border-bottom: 2px solid var(--secondary-color, #ddd);
+      }
     `;
   }
 
   static get properties() {
-    return {};
+    return {
+      userData: { type: Object }
+    };
   }
 
   constructor() {
@@ -21,15 +39,32 @@ class EitUserMenu  extends LitElement {
 
   render() {
     return html`
-      <eit-button @click="${this.doLogout}">
-        logout
-      </eit-button>
+      <dile-menu-overlay>
+        <dile-avatar class="trigger" slot="trigger" initial="${this.getInitial(this.userData)}"></dile-avatar>
+        <div slot="content" class="menucontent">
+          <p>
+            ${this.userData.name}
+          </p>
+          <eit-button @click="${this.doLogout}">
+            logout
+          </eit-button>
+        </div>
+      </dile-menu-overlay>
     `;
   }
 
   doLogout() {
     document.querySelector("#logout-form").submit();
   }
+
+  getInitial(user) {
+    if(user) {
+      return user.name[0];
+    }
+    return '';
+  }
+
+
 }
 
 customElements.define('eit-user-menu', EitUserMenu);
